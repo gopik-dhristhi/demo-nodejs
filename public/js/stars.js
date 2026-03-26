d8216a8e516a3c67495abb867ba242d82eba5aca
Gopi Krishna <gopikrishna@DHRPUN008.local>
2026-03-26 12:13:31 +0530

    modified  deleter star.sc

--- public/js/stars.js
+++ public/js/stars.js
@@ -1,17 +1,17 @@
-const starCount = 200;
+// const starCount = 200;
 
-for (let i = 0; i < starCount; i++) {
-  const star = document.createElement("div");
-  star.className = "star";
+// for (let i = 0; i < starCount; i++) {
+//   const star = document.createElement("div");
+//   star.className = "star";
 
-  const size = Math.random() * 2;
-  star.style.width = size + "px";
-  star.style.height = size + "px";
+//   const size = Math.random() * 2;
+//   star.style.width = size + "px";
+//   star.style.height = size + "px";
 
-  star.style.top = Math.random() * 100 + "vh";
-  star.style.left = Math.random() * 100 + "vw";
+//   star.style.top = Math.random() * 100 + "vh";
+//   star.style.left = Math.random() * 100 + "vw";
 
-  star.style.animationDuration = (Math.random() * 5 + 2) + "s";
+//   star.style.animationDuration = (Math.random() * 5 + 2) + "s";
 
-  document.body.appendChild(star);
-}
+//   document.body.appendChild(star);
+// }
